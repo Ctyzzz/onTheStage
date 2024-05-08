b52abd2ecf0b2260add03b2e70a4d5b9ee981383
Ctyzzz <sentus008@gmail.com>
2024-05-08 03:29:50 +0300

    Header done

--- src/pages/NonAuthPage/NonAuthPage.tsx
+++ src/pages/NonAuthPage/NonAuthPage.tsx
@@ -1,12 +1,32 @@
 import styles from "./NonAuthPage.module.scss"
-import logo from "@public/logo.svg"
+
+import logo from "../../../public/logo.svg"
+
+import EntryBtn from "@src/UI/EntryBtn/EntryBtn"
+import RegBtn from "@src/UI/RegBtn/RegBtn"
+
 
 const NonAuthPage = () => {
     return(
         <>
             <div className={styles.overView}>
-                <div className={styles.header}>
+                <div className={styles["overView__header"]}>
                     <img src={logo} alt="logo" width={159} height={63} />
+                    <ul className={styles["overView__header__menu"]}>
+                        <li>
+                            О нас
+                        </li>
+                        <li>
+                            Тарифы
+                        </li>
+                        <li>
+                            Популярное
+                        </li>
+                    </ul>
+                    <div className={styles["overView__header__buttons"]}>
+                        <EntryBtn></EntryBtn>
+                        <RegBtn></RegBtn>
+                    </div>
                 </div>
             </div>
             <div className={styles.onlyOne}>
