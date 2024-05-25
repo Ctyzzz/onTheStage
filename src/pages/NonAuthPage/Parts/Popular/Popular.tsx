a3f089e088726dc4fa4324cc27f2bb60a5c3e590
Ctyzzz <sentus008@gmail.com>
2024-05-25 12:58:57 +0300

    2 block's added. Swipe in work

--- src/pages/NonAuthPage/Parts/Popular/Popular.tsx
+++ src/pages/NonAuthPage/Parts/Popular/Popular.tsx
@@ -1,10 +1,79 @@
-import styles from "./Popular.module.scss"
+import React, { useRef, useState, useEffect } from "react";
+import styles from "./Popular.module.scss";
+import Card from "@ui/Card/Card";
+import Messiah from "@public/messiah.png";
+import Onegin from "@public/Onegin.png";
+import BoD from "@public/balletofdifference.png";
+import Saul from "@public/Saul.png";
+import Anna from "@public/AnnaBolein.png";
+import PierGunt from "@public/PierGunt.png";
 
 const Popular = () => {
-    return(
-        <>
-        </>
-    )
+    const galleryRef = useRef(null);
+    const [showHint, setShowHint] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowHint(false);
+        }, 1000); // Hide hint after 1 second of the first swipe
+        return () => clearTimeout(timer);
+    }, [showHint]);
+
+    const handleMouseDown = (e) => {
+        setShowHint(false);
+        galleryRef.current.isDown = true;
+        galleryRef.current.startX = e.pageX - galleryRef.current.offsetLeft;
+        galleryRef.current.scrollLeft = galleryRef.current.scrollLeft;
+    };
+
+    const handleMouseLeave = () => {
+        galleryRef.current.isDown = false;
+    };
+
+    const handleMouseUp = () => {
+        galleryRef.current.isDown = false;
+    };
+
+    const handleMouseMove = (e) => {
+        if (!galleryRef.current.isDown) return;
+        e.preventDefault();
+        const x = e.pageX - galleryRef.current.offsetLeft;
+        const walk = x - galleryRef.current.startX;
+        galleryRef.current.scrollLeft = galleryRef.current.scrollLeft - walk;
+    };
+
+    return (
+        <div className={styles.popular}>
+            <div className={styles["popular__title"]}>
+                <h1>Популярное на платформе</h1>
+                <p>Более тысячи спектаклей разных жанров</p>
+            </div>
+            <div 
+                className={styles["popular__cards"]}
+                ref={galleryRef}
+                onMouseDown={handleMouseDown}
+                onMouseLeave={handleMouseLeave}
+                onMouseUp={handleMouseUp}
+                onMouseMove={handleMouseMove}
+            >
+                {showHint && (
+                    <div className={styles.hint}>
+                        <div className={styles.hintElement}></div>
+                        <div className={styles.hintElement}></div>
+                        <div className={styles.hintElement}></div>
+                        <div className={styles.hintElement}></div>
+                        <div className={styles.hintElement}></div>
+                    </div>
+                )}
+                <Card picture={Messiah}></Card>
+                <Card picture={Onegin}></Card>
+                <Card picture={BoD}></Card>
+                <Card picture={Saul}></Card>
+                <Card picture={Anna}></Card>
+                <Card picture={PierGunt}></Card>
+            </div>
+        </div>
+    );
 }
 
 export default Popular;
--- src/pages/NonAuthPage/Parts/Subscription/Subscription.tsx
+++ src/pages/NonAuthPage/Parts/Subscription/Subscription.tsx
@@ -1,9 +1,45 @@
 import styles from "./Subscription.module.scss"
 
+import ico from "@public/check.svg"
+import icorev from "@public/checkreverse.svg"
+
+import TicketsWhite from "@ui/Tickets/White/TicketsWhite"
+import TicketsBlue from "@ui/Tickets/Blue/TicketsBlue"
+
 const Subscription = () => {
     return(
-        <>
-        </>
+        <div className={styles.subscription}>
+            <div className={styles["subscription__variations"]}>
+                <h1>Варианты подписок</h1>
+                <p>Подбери удобный для себя вариант</p>
+            </div>
+            <div className={styles["subscription__ticketss"]}>
+                <TicketsWhite 
+                naming={"тариф <br/> малый театр"}
+                icon={ico} w={24} h={24}
+                about1={"Более 1000 спектаклей <br/> по всему миру"}
+                about2={"Доступ к интерактивным <br/> спектаклям"}
+                about3={"Жизнь театров за кадром"}
+                price={"750 руб/месяц"}>
+                </TicketsWhite>
+                <TicketsWhite
+                naming={"тариф <br/> режиссер"}
+                icon={ico} w={24} h={24}
+                about1={"Возможность собрать <br/> подборку из 4 спектаклей"}
+                about2={"Возможность заглянуть <br/> за кулисы"}
+                about3={"Доступно скачивание"}
+                price={"550 руб/месяц"}>
+                </TicketsWhite>
+                <TicketsBlue
+                naming={"тариф <br/> большой театр"}
+                icon={icorev} w={24} h={24}
+                about1={"Более 1000 спектаклей <br/> по всему миру"}
+                about2={"Доступ ко всем сервисам <br/> платформы"}
+                about3={"Спектакли до премьеры"}
+                price={"7950 руб/месяц"}>
+                </TicketsBlue>
+            </div>
+        </div>
     )
 }
 
